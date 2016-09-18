4fc4b249eee924c164c4ca02bfab60723eb778b9
eranga herath <eranga.herath@pagero.com>
2016-09-18 13:18:16 +0530

    remove multidex optimize google play service usage

--- app/src/main/java/com/score/chatz/application/SenzApplication.java
+++ app/src/main/java/com/score/chatz/application/SenzApplication.java
@@ -1,9 +1,6 @@
 package com.score.chatz.application;
 
 import android.app.Application;
-import android.content.Context;
-import android.support.multidex.MultiDex;
-import android.support.multidex.MultiDexApplication;
 
 import com.score.senzc.pojos.Senz;
 
@@ -40,10 +37,4 @@ public class SenzApplication extends Application {
         this.senz = senz;
     }
 
-
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        MultiDex.install(this);
-    }
 }
