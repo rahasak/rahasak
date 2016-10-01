d6a24382aaff00841c3cbd1aaf20146b9ba023b3
Lakmal Caldera <lakmal.caldera@cslworld.com>
2016-10-01 09:49:19 +0530

    tab layout confirm message

--- app/src/main/java/com/score/chatz/ui/BaseActivity.java
+++ app/src/main/java/com/score/chatz/ui/BaseActivity.java
@@ -136,7 +136,7 @@ public class BaseActivity extends AppCompatActivity implements ISendingComHandle
         //set layout for dialog
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.share_confirm_message_dialog);
-        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         dialog.setCanceledOnTouchOutside(false);
         dialog.setCancelable(true);
 
