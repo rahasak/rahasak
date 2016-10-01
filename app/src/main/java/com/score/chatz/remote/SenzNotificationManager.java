7d3324dd33d980b13ad5d0bfb4072c8a2ff9a67d
eranga herath <eranga.herath@pagero.com>
2016-10-01 21:52:36 +0530

    tweak

--- app/src/main/java/com/score/chatz/remote/SenzNotificationManager.java
+++ app/src/main/java/com/score/chatz/remote/SenzNotificationManager.java
@@ -48,8 +48,8 @@ class SenzNotificationManager {
         } else {
             intent = new Intent(context, HomeActivity.class);
         }
-
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
 
         // build notification
