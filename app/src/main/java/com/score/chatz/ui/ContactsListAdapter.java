ae136e3f05663218e80097117048429892f32242
Lakmal Caldera <lakmal.developer@gmail.com>
2016-11-05 15:28:22 +0530

    refactored contacts list adapter

--- app/src/main/java/com/score/chatz/ui/ContactsListAdapter.java
+++ app/src/main/java/com/score/chatz/ui/ContactsListAdapter.java
@@ -3,8 +3,6 @@ package com.score.chatz.ui;
 import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Typeface;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
@@ -16,74 +14,14 @@ public class ContactsListAdapter extends SimpleCursorAdapter {
 
     private Typeface typeface;
 
-    public ContactsListAdapter(Context context, int layout, Cursor c, String[] from, int[] to) {
-        super(context, layout, c, from, to);
-        typeface = Typeface.createFromAsset(context.getAssets(), "fonts/GeosansLight.ttf");
-    }
-
     public ContactsListAdapter(Context context, int layout, Cursor c, String[] from, int[] to, int flags) {
         super(context, layout, c, from, to, flags);
         typeface = Typeface.createFromAsset(context.getAssets(), "fonts/GeosansLight.ttf");
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
-        super.bindView(view, context, cursor);
-    }
-
-    @Override
-    public ViewBinder getViewBinder() {
-        return super.getViewBinder();
-    }
-
-    @Override
-    public void setViewBinder(ViewBinder viewBinder) {
-        super.setViewBinder(viewBinder);
-    }
-
-    @Override
-    public void setViewImage(ImageView v, String value) {
-        super.setViewImage(v, value);
-    }
-
-    @Override
     public void setViewText(TextView v, String text) {
         super.setViewText(v, text);
         v.setTypeface(typeface);
-    }
-
-    @Override
-    public int getStringConversionColumn() {
-        return super.getStringConversionColumn();
-    }
-
-    @Override
-    public void setStringConversionColumn(int stringConversionColumn) {
-        super.setStringConversionColumn(stringConversionColumn);
-    }
-
-    @Override
-    public CursorToStringConverter getCursorToStringConverter() {
-        return super.getCursorToStringConverter();
-    }
-
-    @Override
-    public void setCursorToStringConverter(CursorToStringConverter cursorToStringConverter) {
-        super.setCursorToStringConverter(cursorToStringConverter);
-    }
-
-    @Override
-    public CharSequence convertToString(Cursor cursor) {
-        return super.convertToString(cursor);
-    }
-
-    @Override
-    public Cursor swapCursor(Cursor c) {
-        return super.swapCursor(c);
-    }
-
-    @Override
-    public void changeCursorAndColumns(Cursor c, String[] from, int[] to) {
-        super.changeCursorAndColumns(c, from, to);
     }
 }
