de9c7d9e21d3569646e2c37697bb507b90cf7721
Lakmal Caldera <lakmal.developer@gmail.com>
2016-12-06 00:32:10 +0530

    added phone number into contacts list

--- app/src/main/java/com/score/chatz/ui/ContactsListAdapter.java
+++ app/src/main/java/com/score/chatz/ui/ContactsListAdapter.java
@@ -6,21 +6,25 @@ import android.graphics.Typeface;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
+import com.score.chatz.utils.PhoneUtils;
+
 /**
  * Created by lakmalcaldera on 11/5/16.
  */
 class ContactsListAdapter extends SimpleCursorAdapter {
 
     private Typeface typeface;
+    private Context _context;
 
     ContactsListAdapter(Context context, int layout, Cursor c, String[] from, int[] to, int flags) {
         super(context, layout, c, from, to, flags);
         typeface = Typeface.createFromAsset(context.getAssets(), "fonts/GeosansLight.ttf");
+        _context = context;
     }
 
     @Override
     public void setViewText(TextView v, String text) {
-        super.setViewText(v, text);
+        super.setViewText(v, text + " / " + PhoneUtils.getNumberFromName(text, _context));
         v.setTypeface(typeface);
     }
 }
--- app/src/main/java/com/score/chatz/utils/PhoneUtils.java
+++ app/src/main/java/com/score/chatz/utils/PhoneUtils.java
@@ -26,4 +26,17 @@ public class PhoneUtils {
 
         return displayName;
     }
+
+    public static String getNumberFromName(String name, Context context) {
+        String phoneNumber = null;
+        String selection = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME+" like'%" + name +"%'";
+        String[] projection = new String[] { ContactsContract.CommonDataKinds.Phone.NUMBER};
+        Cursor c = context.getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                projection, selection, null, null);
+        if (c.moveToFirst()) {
+            phoneNumber = c.getString(0);
+        }
+        c.close();
+        return phoneNumber;
+    }
 }
