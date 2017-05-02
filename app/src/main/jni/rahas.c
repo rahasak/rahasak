702b24c1b30643128a596bccc949ca62fa2ece44
eranga herath <eranga.herath@pagero.com>
2017-05-02 20:24:21 +0530

    removed unwated jni

--- app/src/main/jni/rahas.c
+++
@@ -1,5 +0,0 @@
-#include <rahas.h>
-
-JNIEXPORT jint JNICALL Java_com_score_rahasak_Rahas_addz (JNIEnv * env, jobject obj, jint i) {
-    return 1;
-}
--- app/src/main/jni/rahas.h
+++
@@ -1,3 +0,0 @@
-#include <jni.h>
-
-JNIEXPORT jint JNICALL Java_com_score_rahasak_Rahas_addz (JNIEnv *, jobject, jint);
