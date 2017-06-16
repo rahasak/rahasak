b7578b12addf0a34036f2b7db5a18c0a21ba9ec5
eranga herath <eranga.herath@pagero.com>
2017-06-16 10:41:42 +0530

    refactored time utils

--- app/src/main/java/com/score/rahasak/utils/TimeUtils.java
+++ app/src/main/java/com/score/rahasak/utils/TimeUtils.java
@@ -1,6 +1,7 @@
 package com.score.rahasak.utils;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class TimeUtils {
@@ -11,22 +12,35 @@ public class TimeUtils {
     public static String getTimeInWords(long timestamp) {
         String time = null;
         try {
+            Calendar calendar = Calendar.getInstance();
+
             Date now = new Date();
+            calendar.setTime(now);
+            int nowDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+            Date pre = new Date(timestamp * 1000);
+            calendar.setTime(pre);
+            int preDay = calendar.get(Calendar.DAY_OF_MONTH);
+
             long deltaSeconds = now.getTime() / 1000 - timestamp;
 
             if (deltaSeconds < 10) {
+                // just now
                 time = "Now";
             } else if (deltaSeconds < 60) {
+                // one minute ago
                 time = "1 minute ago";
             } else if (deltaSeconds < (60 * 60)) {
+                // few minutes ago
                 time = deltaSeconds / 60 + " minutes ago";
-            } else if (deltaSeconds < (60 * 60 * 24 * 2)) {
-                if (DATE_FORMAT.format(new Date(timestamp * 1000)).equalsIgnoreCase(DATE_FORMAT.format(now))) {
-                    time = TIME_FORMAT.format(new Date(timestamp * 1000));
-                } else {
-                    time = "Yesterday, " + TIME_FORMAT.format(new Date(timestamp * 1000));
-                }
+            } else if (nowDay - preDay == 0) {
+                // today
+                time = TIME_FORMAT.format(new Date(timestamp * 1000));
+            } else if (nowDay - preDay == 1) {
+                // yesterday
+                time = "Yesterday, " + TIME_FORMAT.format(new Date(timestamp * 1000));
             } else {
+                // before yesterday
                 time = DATE_TIME_FORMAT.format(new Date(timestamp * 1000));
             }
         } catch (Exception ex) {
